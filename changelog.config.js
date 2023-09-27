15f26fb02af102d0c0794ab53b85819d75755864
monolith-junhyoung <junhyoung.lee@monolith.co.kr>
2023-09-27 13:49:39 +0900

    chore: added configuration for ChangeLog

---
+++ changelog.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    "outputWriter": "conventional-commits-markdown",
+    "types": [
+      { "type": "feat", "section": "Features" },
+      { "type": "fix", "section": "Bug Fixes" },
+      { "type": "refactor", "section": "Refactors" },
+      { "type": "*", "hidden": true }
+    ]
+  };
